f53e0ab8e7a43b5f450a8c9070a4798bd2a72683
Nitin Sahu <sahunitin970@gmail.com>
2023-07-06 21:13:46 +0530

    SIDCUP FAMILY GOLF CLONE HERO SECTION READY

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('mousemove', function (dets) {
+    console.log(dets)
+})
+
+gsap.to('#nav', {
+    backgroundColor: '#000',
+    height: "120px",
+    duration: 0.5,
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: 'body',
+        // markers: true,
+        start: 'top -10%',
+        end: 'top -11%',
+        scrub: 1
+    }
+})
+
+gsap.to('#main', {
+    backgroundColor: '#000',
+    scrollTrigger: {
+        trigger: '#main',
+        scroller: 'body',
+        markers: true,
+        start: 'top -20%',
+        end: 'top -70%',
+        scrub: 1
+    }
+})
